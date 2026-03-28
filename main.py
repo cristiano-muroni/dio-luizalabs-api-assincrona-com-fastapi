6c4967aa299f84a91f113af8bdb716166a230fa9
Cristiano Muroni <muronidev@gmail.com>
2026-03-28 17:47:18 -0300

    rotes, query, parameters, etc

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 
+from  datetime import datetime, UTC 
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello world!"}
+fake_db = [
+    {"title": f"Criando uma aplicação com Django", "date": datetime.now(UTC),'published': True},
+    {"title": f"Intercionalizando uma aplicação com Flask", "date": datetime.now(UTC), 'published': True},
+    {"title": f"Criando uma aplicação com FastAPI", "date": datetime.now(UTC), 'published': True},
+    {"title": f"Hands-on de uma aplicação com Pandas", "date": datetime.now(UTC), 'published': False}
+] # type: ignore
+
+@app.get("/posts")
+def read_posts_pub(skip: int = 0 , limit: int = len(fake_db), published: bool = True):
+    return [post for post in fake_db[skip: skip + limit] if post['published'] is published]
+
+@app.get("/posts/{framework}")
+def read_posts(framework: int):
+    return {"posts": [
+        {"title": f"Criando uma aplicação com {framework}", "date": datetime.now(UTC)},
+        {"title": f"Intercionalizando uma aplicação com {framework}", "date": datetime.now(UTC)}
+        ]}
