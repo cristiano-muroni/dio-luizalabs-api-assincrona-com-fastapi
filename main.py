0dc7fffacbff78ae155510ca70672ff8d098c8f4
Cristiano Muroni <muronidev@gmail.com>
2026-03-29 18:10:47 -0300

    query anderequests body

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from  datetime import datetime, UTC 
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,9 +13,25 @@ fake_db = [
     {"title": f"Hands-on de uma aplicação com Pandas", "date": datetime.now(UTC), 'published': False}
 ] # type: ignore
 
+class Post(BaseModel):
+    title: str
+    date: datetime = datetime.now(UTC)
+    published: bool = False   
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def crate_post(post: Post):
+    fake_db.append(post.model_dump())
+    return post
+
 @app.get("/posts")
-def read_posts_pub(skip: int = 0 , limit: int = len(fake_db), published: bool = True):
-    return [post for post in fake_db[skip: skip + limit] if post['published'] is published]
+def read_posts_pub(published: bool, limit: int, skip: int =0 ):
+    posts = []
+    for post in fake_db:
+        if len(posts) is limit:
+            break
+        if post["published"] is published:
+            posts.append(post)
+    return posts # [post for post in fake_db[skip: skip + limit] if post['published'] is published]
 
 @app.get("/posts/{framework}")
 def read_posts(framework: int):
