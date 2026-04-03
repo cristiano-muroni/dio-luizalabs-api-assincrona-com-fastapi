e252a431e1e54cfa11c343fdb06b2e2780a19211
Cristiano Muroni <muronidev@gmail.com>
2026-04-03 12:28:59 -0300

    function read posts

--- controllers/post.py
+++ controllers/post.py
@@ -1,22 +1,22 @@
 from fastapi import APIRouter, status, Response
 from schemas.post import PostIn
 from views.post import PostOut
-
+from models.post import posts
+from database import database
 
 router = APIRouter(prefix="/posts")
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 def crate_post(post: PostIn):
-    # fake_db.append(post.model_dump())
+    # ---old fake_db
     return post
 
 @router.get("/",response_model=list[PostOut])
-def read_posts_pub(
+async def read_posts(
     response: Response,
     published: bool,
     limit: int,
     skip: int =0    
     ):
-    return []
-    # tail = skip + limit   
-    # [post for post in fake_db[skip: tail] if post['published'] is published]
+    query = posts.select()
+    return  await database.fetch_all(query)
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import databases
-import sqlalchemy as sa
 from fastapi import FastAPI
 from controllers import post
 from contextlib import asynccontextmanager
+from database import database, engine, metadata
 
-DATABASE_URL = "sqlite:///./blog.db"
 
-database = databases.Database(DATABASE_URL)
-metadata = sa.MetaData()
-engine = sa.create_engine(DATABASE_URL, connect_args= {"check_same_thread": False})
 
 @asynccontextmanager
 async def Lifespan(app: FastAPI):
