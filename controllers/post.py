7004053acc5371fb567e23d5800f009a116e8d06
Cristiano Muroni <muronidev@gmail.com>
2026-04-03 12:33:56 -0300

    cleanning function read posts

--- controllers/post.py
+++ controllers/post.py
@@ -1,22 +1,18 @@
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status
 from schemas.post import PostIn
 from views.post import PostOut
 from models.post import posts
 from database import database
 
+
 router = APIRouter(prefix="/posts")
+
+@router.get("/", response_model=list[PostOut])
+async def read_posts(published: bool, limit: int, skip: int = 0):
+    query = posts.select()
+    return await database.fetch_all(query)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 def crate_post(post: PostIn):
     # ---old fake_db
     return post
-
-@router.get("/",response_model=list[PostOut])
-async def read_posts(
-    response: Response,
-    published: bool,
-    limit: int,
-    skip: int =0    
-    ):
-    query = posts.select()
-    return  await database.fetch_all(query)
